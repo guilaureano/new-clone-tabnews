d47cfb70ed784e558fa091866425aa5e17c7eefb
Guilherme Medeiros Laureano <guilaureano.dev@gmail.com>
2026-09-15 20:05:21 -0300

    add file `jsconfig` and adjust baseUrl

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,9 @@
-import database from '../../../../infra/database.js'
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
-  console.log('Database:', result.rows)
-  return response.status(200).json({ chave: 'online' })
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log("Database:", result.rows);
+  return response.status(200).json({ chave: "online" });
 }
 
-export default status
+export default status;
